33ac6d6a152132de26c6e7b61162c77e91784335
Alexander Fabisch <alexander.fabisch@dfki.de>
2018-02-12 09:54:22 +0100

    Fetch dependencies

--- build_tools/install_externals.sh
+++ build_tools/install_externals.sh
@@ -1,10 +1,7 @@
 if [ -d "$DIRECTORY" ]; then
     cp -rf /CDFF/External/install ../CDFF/External/install
 else
-    echo "Currently there is no way to install dependencies automatically."
-    echo "Please follow the instructions on the readme of CDFF:"
-    echo ""
-    echo "    https://gitlab.spaceapplications.com/InFuse/CDFF/tree/master#get-all-dependencies-first"
-    echo ""
+    cd ../CDFF/External/
+    ./fetch_compile_install_dependencies.sh
 fi
 
