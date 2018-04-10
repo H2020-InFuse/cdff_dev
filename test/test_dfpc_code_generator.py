2fd813f1385ca1f0ce7b3f665d714dbceb580620
Alexander Fabisch <alexander.fabisch@dfki.de>
2018-04-10 12:05:37 +0200

    Test Python bindings

--- test/test_dfpc_code_generator.py
+++ test/test_dfpc_code_generator.py
@@ -41,7 +41,7 @@ def test_compile():
 
         incdirs = ["test/test_output/", "CDFF/DFPCs", ctypespath, dfpcspath]
         build_extension(
-            tmp_folder, hide_stderr=False,
+            tmp_folder, hide_stderr=True,
             name=node["name"].lower(),
             pyx_filename=os.path.join(
                 tmp_folder, "python", node["name"].lower() + ".pyx"),
