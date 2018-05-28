1f5049b7020b3fc8f5acdf2da260b618f5043bbe
Alexander Fabisch <alexander.fabisch@dfki.de>
2018-05-28 16:57:37 +0200

    Extract control GUI

--- cdff_dev/envirevisualization.py
+++ cdff_dev/envirevisualization.py
@@ -145,20 +145,3 @@ class Step:
         obj = typefromdict.create_from_dict(typename, sample)
         self.dfc.process_sample(
             timestamp=timestamp, stream_name=stream_name, sample=obj)
-
-
-if __name__ == "__main__":
-    class Work:
-        def __init__(self):
-            self.i = 0
-
-        def __call__(self):
-            print(self.i)
-            self.i += 1
-            if self.i > 10000:
-                raise StopIteration()
-
-    app = QApplication(sys.argv)
-    win = ReplayMainWindow(app, Work)
-    win.show()
-    app.exec_()
---
+++ examples/control_gui.py
@@ -0,0 +1,21 @@
+import sys
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from cdff_dev import envirevisualization
+
+
+if __name__ == "__main__":
+    class Work:
+        def __init__(self):
+            self.i = 0
+
+        def __call__(self):
+            print(self.i)
+            self.i += 1
+            if self.i > 10000:
+                raise StopIteration()
+
+    app = QApplication(sys.argv)
+    win = envirevisualization.ReplayMainWindow(app, Work)
+    win.show()
+    app.exec_()
