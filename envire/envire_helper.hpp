7e63608c95dd5c799159f0d5ea7a9d1521892607
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2018-06-20 16:40:14 +0200

    use envire_asn1 plugin to create types

--- envire/envire_helper.hpp
+++ envire/envire_helper.hpp
@@ -6,8 +6,10 @@
 #include <envire_urdf/GraphLoader.hpp>
 #include <urdf_parser/urdf_parser.h>
 #include <envire_core/plugin/Plugin.hpp>
+#include <envire_core/plugin/ClassLoader.hpp>
 #include <envire_core/items/Item.hpp>
-
+#include <envire_core/util/Demangle.hpp>
+#include <boost/pointer_cast.hpp>
 
 template <class _ItemData>
 unsigned getItemCount(
@@ -20,55 +22,96 @@ unsigned getItemCount(
 #include <iostream>
 class GenericItem
 {
-    void* ptr;
+    //void* ptr;
+    envire::core::ItemBase::Ptr item;
 public:
-    GenericItem() : ptr(0)
+    GenericItem()
     {
     }
 
     ~GenericItem()
     {
-        if(ptr)
-            throw std::runtime_error("Item content has not been deleted.");
+        // if(ptr)
+        //     throw std::runtime_error("Item content has not been deleted.");
     }
 
     template <class _ItemData>
     void initialize(_ItemData* content)
     {
-        typename envire::core::Item<_ItemData>::Ptr* typedPtr =
-            new typename envire::core::Item<_ItemData>::Ptr(
-                new envire::core::Item<_ItemData>(*content));
-        ptr = (void*) typedPtr;
+        
+        std::string name = envire::core::demangleTypeName(typeid(*content));
+        std::string itemname = "envire::core::Item<"+name+">";
+
+        envire::core::ClassLoader* loader = envire::core::ClassLoader::getInstance();
+            if(loader->hasClass(itemname))
+            {
+                if (loader->createEnvireItem(itemname, item))
+                {
+                    printf("created %s\n",itemname.c_str()); 
+                    
+                    // //typename envire::core::Item<_ItemData>::Ptr* typedPtr = (typename envire::core::Item<_ItemData>::Ptr *) ptr;
+                    typename envire::core::Item<_ItemData>::Ptr typedPtr = boost::dynamic_pointer_cast< typename envire::core::Item<_ItemData> >(item);
+                    // ptr = (void*) &typedPtr;
+                    typedPtr->setData(*content);
+                    typedPtr->contentsChanged();
+
+                }
+            }else{
+                printf("no class with name %s found in plugins\n",name.c_str());
+            }
+        //     //ok
+        //     envire::core::Item<_ItemData> *ritem = new envire::core::Item<_ItemData>(*content);
+        //     //ok
+        //     typename envire::core::Item<_ItemData>::Ptr* typedPtr = new typename envire::core::Item<_ItemData>::Ptr(ritem);
+        //     //nope
+        //     envire::core::ItemBase* base = dynamic_cast<envire::core::ItemBase*>(ritem);
+        //     //nope
+        //     item = boost::dynamic_pointer_cast< envire::core::ItemBase > (*typedPtr);
+        //     ptr = (void*) typedPtr;
+
+        // std::cout << __PRETTY_FUNCTION__ << "\n\t" << (*typedPtr)->getRawData() << " " << item->getRawData() << " " << base->getRawData() << " " << ritem->getRawData() << std::endl;
+        // std::cout << __PRETTY_FUNCTION__ << "\n\t" << (*typedPtr)->getID() << " " << item->getID() << std::endl;
+
     }
 
     template <class _ItemData>
     typename envire::core::Item<_ItemData>::Ptr getItem(_ItemData* content)
     {
-        return *((typename envire::core::Item<_ItemData>::Ptr *) ptr);
+        //return *((typename envire::core::Item<_ItemData>::Ptr *) ptr);
+        return boost::dynamic_pointer_cast< typename envire::core::Item<_ItemData> >(item);
     }
 
     template <class _ItemData>
     void setData(_ItemData* content)
     {
-        typename envire::core::Item<_ItemData>::Ptr* typedPtr = (typename envire::core::Item<_ItemData>::Ptr *) ptr;
-        (*typedPtr)->setData(*content);
-        (*typedPtr)->contentsChanged();
+        //typename envire::core::Item<_ItemData>::Ptr* typedPtr = (typename envire::core::Item<_ItemData>::Ptr *) ptr;
+        typename envire::core::Item<_ItemData>::Ptr typedPtr = boost::dynamic_pointer_cast< typename envire::core::Item<_ItemData> >(item);
+        typedPtr->setData(*content);
+        typedPtr->contentsChanged();
     }
 
     template <class _ItemData>
     void setTime(_ItemData* content, int64_t timestamp)
     {
-        typename envire::core::Item<_ItemData>::Ptr* typedPtr = (typename envire::core::Item<_ItemData>::Ptr *) ptr;
+        //assert(boost::dynamic_pointer_cast< typename envire::core::Item<_ItemData> >(item) != 0);
+        //typename envire::core::Item<_ItemData>::Ptr* typedPtr = (typename envire::core::Item<_ItemData>::Ptr *) ptr;
+        typename envire::core::Item<_ItemData>::Ptr typedPtr = boost::dynamic_pointer_cast< typename envire::core::Item<_ItemData> >(item);
         base::Time time;
         time.microseconds = timestamp;
-        (*typedPtr)->setTime(time);
+        typedPtr->setTime(time);
+    }
+
+    template <class _ItemData>
+    std::string getID()
+    {
+        return item->getID();
     }
 
     template <class _ItemData>
     void deleteItem(_ItemData* content)
     {
-        delete (typename envire::core::Item<_ItemData>::Ptr *) ptr;
-        ptr = 0;
+        // delete (typename envire::core::Item<_ItemData>::Ptr *) ptr;
+        // ptr = 0;
     }
 };
 
@@ -97,22 +140,3 @@ void loadURDF(envire::core::EnvireGraph& graph, const std::string& filename,
         loader.loadJoints(*model);
 }
 
-#define ENVIRE_REGISTER_MULTI_METADATA( _classname, _datatype, _variable ) \
-static envire::core::MetadataInitializer _metadataInit##_variable(typeid(_classname), #_datatype, #_classname);
-#define ENVIRE_REGISTER_MULTI_ITEM_WITHOUT_SERIALIZATION( _datatype, _variable ) \
-CLASS_LOADER_REGISTER_CLASS(envire::core::Item<_datatype>, envire::core::ItemBase) \
-ENVIRE_REGISTER_MULTI_METADATA(envire::core::Item<_datatype>, _datatype, _variable)
-
-
-#include <base/samples/RigidBodyState.hpp>
-ENVIRE_REGISTER_MULTI_ITEM_WITHOUT_SERIALIZATION(base::samples::RigidBodyState, 1)
-#include <base/samples/Pointcloud.hpp>
-ENVIRE_REGISTER_MULTI_ITEM_WITHOUT_SERIALIZATION(base::samples::Pointcloud, 2)
-#include <base/samples/LaserScan.hpp>
-ENVIRE_REGISTER_MULTI_ITEM_WITHOUT_SERIALIZATION(base::samples::LaserScan, 3)
-#include <base/samples/Joints.hpp>
-ENVIRE_REGISTER_MULTI_ITEM_WITHOUT_SERIALIZATION(base::samples::Joints, 4)
-#include <urdf_model/joint.h>
-ENVIRE_REGISTER_MULTI_ITEM_WITHOUT_SERIALIZATION(urdf::Joint, 5)
-#include <urdf_model/link.h>
-ENVIRE_REGISTER_MULTI_ITEM_WITHOUT_SERIALIZATION(urdf::Link, 6)
