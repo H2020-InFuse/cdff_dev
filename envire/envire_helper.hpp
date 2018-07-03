aaaa4284cc965e81b10bcb05295f42fb5a1876dd
Alexander Fabisch <alexander.fabisch@dfki.de>
2018-07-03 15:38:54 +0200

    Remove comments

--- envire/envire_helper.hpp
+++ envire/envire_helper.hpp
@@ -22,7 +22,6 @@ unsigned getItemCount(
 #include <iostream>
 class GenericItem
 {
-    //void* ptr;
     envire::core::ItemBase::Ptr item;
 public:
     GenericItem()
@@ -31,8 +30,6 @@ public:
 
     ~GenericItem()
     {
-        // if(ptr)
-        //     throw std::runtime_error("Item content has not been deleted.");
     }
 
     template <class _ItemData>
@@ -47,11 +44,8 @@ public:
             {
                 if (loader->createEnvireItem(itemname, item))
                 {
-                    printf("created %s\n",itemname.c_str()); 
-                    
-                    // //typename envire::core::Item<_ItemData>::Ptr* typedPtr = (typename envire::core::Item<_ItemData>::Ptr *) ptr;
+                    printf("created %s\n",itemname.c_str());
                     typename envire::core::Item<_ItemData>::Ptr typedPtr = boost::dynamic_pointer_cast< typename envire::core::Item<_ItemData> >(item);
-                    // ptr = (void*) &typedPtr;
                     typedPtr->setData(*content);
                     typedPtr->contentsChanged();
 
@@ -59,32 +53,17 @@ public:
             }else{
                 printf("no class with name %s found in plugins\n",name.c_str());
             }
-        //     //ok
-        //     envire::core::Item<_ItemData> *ritem = new envire::core::Item<_ItemData>(*content);
-        //     //ok
-        //     typename envire::core::Item<_ItemData>::Ptr* typedPtr = new typename envire::core::Item<_ItemData>::Ptr(ritem);
-        //     //nope
-        //     envire::core::ItemBase* base = dynamic_cast<envire::core::ItemBase*>(ritem);
-        //     //nope
-        //     item = boost::dynamic_pointer_cast< envire::core::ItemBase > (*typedPtr);
-        //     ptr = (void*) typedPtr;
-
-        // std::cout << __PRETTY_FUNCTION__ << "\n\t" << (*typedPtr)->getRawData() << " " << item->getRawData() << " " << base->getRawData() << " " << ritem->getRawData() << std::endl;
-        // std::cout << __PRETTY_FUNCTION__ << "\n\t" << (*typedPtr)->getID() << " " << item->getID() << std::endl;
-
     }
 
     template <class _ItemData>
     typename envire::core::Item<_ItemData>::Ptr getItem(_ItemData* content)
     {
-        //return *((typename envire::core::Item<_ItemData>::Ptr *) ptr);
         return boost::dynamic_pointer_cast< typename envire::core::Item<_ItemData> >(item);
     }
 
     template <class _ItemData>
     void setData(_ItemData* content)
     {
-        //typename envire::core::Item<_ItemData>::Ptr* typedPtr = (typename envire::core::Item<_ItemData>::Ptr *) ptr;
         typename envire::core::Item<_ItemData>::Ptr typedPtr = boost::dynamic_pointer_cast< typename envire::core::Item<_ItemData> >(item);
         typedPtr->setData(*content);
         typedPtr->contentsChanged();
@@ -93,8 +72,6 @@ public:
     template <class _ItemData>
     void setTime(_ItemData* content, int64_t timestamp)
     {
-        //assert(boost::dynamic_pointer_cast< typename envire::core::Item<_ItemData> >(item) != 0);
-        //typename envire::core::Item<_ItemData>::Ptr* typedPtr = (typename envire::core::Item<_ItemData>::Ptr *) ptr;
         typename envire::core::Item<_ItemData>::Ptr typedPtr = boost::dynamic_pointer_cast< typename envire::core::Item<_ItemData> >(item);
         base::Time time;
         time.microseconds = timestamp;
@@ -110,8 +87,6 @@ public:
     template <class _ItemData>
     void deleteItem(_ItemData* content)
     {
-        // delete (typename envire::core::Item<_ItemData>::Ptr *) ptr;
-        // ptr = 0;
     }
 };
 
