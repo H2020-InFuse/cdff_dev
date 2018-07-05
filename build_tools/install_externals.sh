9251248399542e810be24cf1fe8bf00e49bd655f
Alexander Fabisch <alexander.fabisch@dfki.de>
2018-07-05 11:54:51 +0200

    Keep CDFF types

--- build_tools/install_externals.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#alexander.fabisch@dfki.de
-if [ -d "/CDFF/External/install" ]; then
-    cp -rf /CDFF/External/install ../CDFF/External/install
-else
-    cd ../CDFF/External/
-    ./fetch_compile_install_dependencies.sh
-fi
-
