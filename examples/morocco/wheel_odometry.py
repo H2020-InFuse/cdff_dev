705ea777e082d02d33ea7e188573a32b5364a1cf
Alexander Fabisch <alexander.fabisch@dfki.de>
2018-08-21 16:34:21 +0200

    Remove GPS conversion

--- examples/morocco/wheel_odometry.py
+++ examples/morocco/wheel_odometry.py
@@ -25,28 +25,17 @@ class Transformer(transformer.EnvireDFN):
         self.imu_initialized = False
         self.start_pos = np.zeros(3)
 
-    def gpsPos2globalPose0Input(self, data):
-        self._set_transform(data, frame_transformation=False)
-
     def imu02GlobalPose0Input(self, data):
         if not self.imu_initialized:
             self.start_pos = data.pos.toarray()
             self.start_orient = data.orient.toarray()
 
-            t = cdff_types.RigidBodyState()
-            t.pos.fromarray(np.zeros(3))
-            t.orient.fromarray(data.orient.toarray())
-            t.source_frame = "global_pose0"
-            t.target_frame = "imu0"
-            t.timestamp.microseconds = self._timestamp
-            self._set_transform(t, frame_transformation=False)
-
             self.imu_initialized = True
 
         t = cdff_types.RigidBodyState()
         p, q = _subtract_pose(
-            data.pos.toarray(), data.orient.toarray(), self.start_pos,
-            self.start_orient)
+            data.pos.toarray(), data.orient.toarray(),
+            self.start_pos, self.start_orient)
         t.pos.fromarray(p)
         t.orient.fromarray(q)
         t.source_frame = "ground_truth"
@@ -54,16 +43,12 @@ class Transformer(transformer.EnvireDFN):
         t.timestamp.microseconds = self._timestamp
         self._set_transform(t, frame_transformation=False)
 
-    def gpsPos2odometryOutput(self):
-        return self._get_transform(
-            "gps_pos", "odometry", frame_transformation=False)
+    def body2odometryInput(self, data):
+        self._set_transform(data, frame_transformation=False)
 
     def groundTruth2OdometryOutput(self):
         return self._get_transform(
             "ground_truth", "odometry", frame_transformation=False)
-
-    def body2odometryInput(self, data):
-        self._set_transform(data, frame_transformation=False)
 
     def process(self):
         super(Transformer, self).process()
@@ -87,73 +72,10 @@ def _quaternion_product(q1, q2):
     return q
 
 
-class GpsToRelativePoseDFN:
-    def __init__(self):
-        self.gps = cdff_types.GpsSolution()
-        self.relative_pose = cdff_types.RigidBodyState()
-
-        self.initial_pos = (0.0, 0.0, 0.0)
-        self.initial_pos_set = False
-        self.input_provided = False
-
-    def set_configuration_file(self, filename):
-        pass
-
-    def configure(self):
-        pass
-
-    def gpsInput(self, gps):
-        self.gps = gps
-        self.input_provided = True
-
-    def process(self):
-        if not self.input_provided:
-            return
-        try:
-            if not self.initial_pos_set:
-                self.initial_pos = self._to_coordinates(self.gps)
-                self.initial_pos_set = True
-
-            self.relative_pose.timestamp.microseconds = self.gps.time.microseconds
-            self.relative_pose.pos.fromarray(
-                np.array(self._to_coordinates(self.gps, self.initial_pos)))
-            self.relative_pose.source_frame = "gps_pos"
-            self.relative_pose.target_frame = "global_pose0"
-            self.relative_pose.orient.fromarray(np.array([0.0, 0.0, 0.0, 1.0]))
-            self.relative_pose.cov_position[0, 0] = \
-                self.gps.deviation_latitude ** 2
-            self.relative_pose.cov_position[1, 1] = \
-                self.gps.deviation_longitude ** 2
-            self.relative_pose.cov_position[2, 2] = \
-                self.gps.deviation_altitude ** 2
-        except Exception as e:
-            warnings.warn("Failure with GPS coordinates latitude=%g, "
-                          "longitude=%g, altitude=%g, error: %s"
-                          % (self.gps.latitude, self.gps.longitude,
-                             self.gps.altitude, e))
-
-    def gpsPos2globalPose0Output(self):
-        return self.relative_pose
-
-    def _to_coordinates(self, gps, initial_pos=(0.0, 0.0, 0.0)):
-        """Transform GPS coordinates to x, y, z coordinates."""
-        easting, northing, altitude = conversion.convert_to_utm(
-            gps.latitude, gps.longitude, gps.altitude,
-            utm_zone=utm_zone, utm_north=utm_north)
-
-        # NWU
-        northing, westing, up = conversion.convert_to_nwu(
-            easting, northing, altitude,
-            initial_pos[0], initial_pos[1], initial_pos[2])
-        return northing, westing, up
-
-
 class EvaluationDFN:
     def __init__(self):
         self.odometry_timestamps_ = []
         self.odometry_positions_ = []
-        self.dgps_timestamps_ = []
-        self.dgps_positions_ = []
         self.ground_truth_timestamps_ = []
         self.ground_truth_positions_ = []
         self.error = 0.0
@@ -163,11 +85,6 @@ class EvaluationDFN:
 
     def configure(self):
         pass
-
-    def gpsPos2odometryInput(self, gps_pose):
-        if gps_pose.timestamp.microseconds > 0:
-            self.dgps_timestamps_.append(gps_pose.timestamp.microseconds)
-            self.dgps_positions_.append(gps_pose.pos.toarray())
 
     def body2odometryInput(self, odometry_pose):
         if odometry_pose.timestamp.microseconds > 0:
@@ -183,10 +100,10 @@ class EvaluationDFN:
         return self.error
 
     def process(self):
-        if not self.odometry_positions_ or not self.dgps_positions_:
+        if not self.odometry_positions_ or not self.ground_truth_positions_:
             return
         self.error = np.linalg.norm(self.odometry_positions_[-1] -
-                                    self.dgps_positions_[-1])
+                                    self.ground_truth_positions_[-1])
         print("[Evaluation] position error: %.2f" % self.error)
 
 
@@ -207,14 +124,12 @@ def convert_logs():
     # TODO chunk logfiles
 
     # logs/Sherpa/dgps.msg
-    # - /dgps.gps_solution
     # - /dgps.imu_pose
     # logs/Sherpa/sherpa_tt_mcs.msg
     # - /mcs_sensor_processing.rigid_body_state_out
 
     logfiles = [["logs/Sherpa/dgps.msg"], ["logs/Sherpa/sherpa_tt_mcs.msg"]]
     stream_names = [
-       "/dgps.gps_solution",
        "/dgps.imu_pose",
        "/mcs_sensor_processing.rigid_body_state_out",
     ]
@@ -224,7 +139,6 @@ def convert_logs():
 
 def configure(logs, stream_names):
     nodes = {
-        "gps_to_relative_pose": GpsToRelativePoseDFN(),
         "transformer": Transformer(),
         "evaluation": EvaluationDFN()
         # TODO conversion to path
@@ -232,19 +146,20 @@ def configure(logs, stream_names):
     periods = {
         # frequency of odometry: 0.01
         # frequency of gps: 0.05
-        "gps_to_relative_pose": 0.05,  # TODO
         "transformer": 0.01,
         "evaluation": 1.0, # TODO
     }
     connections = (
+        # inputs to transformer
         ("/dgps.imu_pose", "transformer.imu02GlobalPose0"),
-        ("/dgps.gps_solution", "gps_to_relative_pose.gps"),
-        ("gps_to_relative_pose.gpsPos2globalPose0", "transformer.gpsPos2globalPose0"),
         ("/mcs_sensor_processing.rigid_body_state_out", "transformer.body2odometry"),
-        ("transformer.gpsPos2odometry", "evaluation.gpsPos2odometry"),
+
+        # inputs to evaluation
         ("/mcs_sensor_processing.rigid_body_state_out", "evaluation.body2odometry"),
-        ("evaluation.error", "result.error"),
         ("transformer.groundTruth2Odometry", "evaluation.groundTruth2Odometry"),
+
+        # outputs
+        ("evaluation.error", "result.error"),
     )
     frames = {}  # TODO?
     urdf_files = [
@@ -275,12 +190,8 @@ def configure(logs, stream_names):
     # imu0 - imu sensor frame on the robot, use to connect body0 to initial imu
     #        measurements
     graph.add_frame("imu0")
-    # TODO
+    # dgps0 - initial GPS position with axes aligned to north, west, up
     graph.add_frame("dgps0")
-    # global_pose0 - initial GPS position with axes aligned to north, west, up
-    graph.add_frame("global_pose0")
-    # gps_pos - current GPS position
-    graph.add_frame("gps_pos")
 
     # body - odometry, variable, updated continuously
     # body0 - odometry, constant, known before start
@@ -291,18 +202,17 @@ def configure(logs, stream_names):
     # body0 - imu0, constant, known before start
     t = cdff_envire.Transform()
     t.transform.translation.fromarray(np.array([-0.185, 0.3139, 0.04164]))
-    R = pr.matrix_from_euler_xyz([0.44, 2.225, 0.0])
-    imu2body_wxyz = pr.quaternion_from_matrix(R.T)
+    R = pr.matrix_from_euler_zyx([0.44, 2.225, 0.0])
+    imu2body_wxyz = pr.quaternion_from_matrix(R)
     imu2body_xyzw = np.hstack((imu2body_wxyz[1:], [imu2body_wxyz[0]]))
     t.transform.orientation.fromarray(imu2body_xyzw)
     graph.add_transform("imu0", "body0", t)
     # body0 - dgps0, constant, known before start
     t = cdff_envire.Transform()
     t.transform.translation.fromarray(np.array([0.3, 0.0, -0.53]))
-    t.transform.orientation.fromarray(np.array([0.0, 0.0, 0.0, 1.0]))
+    #t.transform.orientation.fromarray(np.array([0.0, 0.0, 0.0, 1.0]))
+    t.transform.orientation.fromarray(np.array([0.0, 0.0, 0.26067301, -0.96542715]))
     graph.add_transform("dgps0", "body0", t)
-    # imu0 - global_pose0, constant, known after start
-    # gps_pos - global_pose0, variable, updated continuously
 
     # TODO visualize covariance?
 
@@ -312,7 +222,6 @@ def configure(logs, stream_names):
 
 
 def evaluate(dfc):
-    dgps = dfc.nodes["evaluation"].dgps_positions_
     odometry = dfc.nodes["evaluation"].odometry_positions_
     ground_truth = dfc.nodes["evaluation"].ground_truth_positions_
     import matplotlib.pyplot as plt
@@ -320,7 +229,6 @@ def evaluate(dfc):
     plt.figure(figsize=(10, 5))
     plt.subplot(aspect="equal")
     plt.grid()
-    plt.plot([p[0] for p in dgps], [p[1] for p in dgps], label="DGPS")
     plt.plot([p[0] for p in ground_truth], [p[1] for p in ground_truth], label="Ground Truth")
     plt.plot([p[0] for p in odometry], [p[1] for p in odometry], label="Odometry")
     plt.legend()
