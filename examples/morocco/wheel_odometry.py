83baf3034261f1212a1fdd11e79c74fd7b518ec9
Alexander Fabisch <alexander.fabisch@dfki.de>
2018-08-21 11:28:16 +0200

    Subtract initial pose from ground truth

--- examples/morocco/wheel_odometry.py
+++ examples/morocco/wheel_odometry.py
@@ -23,14 +23,15 @@ class Transformer(transformer.EnvireDFN):
     def __init__(self):
         super(Transformer, self).__init__()
         self.imu_initialized = False
-        self.initial_pos = np.zeros(3)
+        self.start_pos = np.zeros(3)
 
     def gpsPos2globalPose0Input(self, data):
         self._set_transform(data, frame_transformation=False)
 
     def imu02GlobalPose0Input(self, data):
         if not self.imu_initialized:
-            self.initial_pos = data.pos.toarray()
+            self.start_pos = data.pos.toarray()
+            self.start_orient = data.orient.toarray()
 
             t = cdff_types.RigidBodyState()
             t.pos.fromarray(np.zeros(3))
@@ -42,11 +43,12 @@ class Transformer(transformer.EnvireDFN):
 
             self.imu_initialized = True
 
-        # TODO initialization of rotation
         t = cdff_types.RigidBodyState()
-        t.pos.fromarray(data.pos.toarray() - self.initial_pos)
-        t.orient.fromarray(data.orient.toarray())
-
+        p, q = _subtract_pose(
+            data.pos.toarray(), data.orient.toarray(), self.start_pos,
+            self.start_orient)
+        t.pos.fromarray(p)
+        t.orient.fromarray(q)
         t.source_frame = "ground_truth"
         t.target_frame = "dgps0"
         t.timestamp.microseconds = self._timestamp
@@ -65,6 +67,24 @@ class Transformer(transformer.EnvireDFN):
 
     def process(self):
         super(Transformer, self).process()
+
+
+def _subtract_pose(p1, q1, p2, q2):
+    p = p1 - p2
+    q = _quaternion_product(q1, _quaternion_inverse(q2))
+    return p, q
+
+
+def _quaternion_inverse(initial_q):
+    # quaternion conjugate is the inverse for unit quaternions
+    return np.hstack((-initial_q[:3], (initial_q[3],)))
+
+
+def _quaternion_product(q1, q2):
+    q = np.empty(4)
+    q[0] = q1[-1] * q2[-1] - np.dot(q1[:3], q2[:3])
+    q[1:] = q1[-1] * q2[:3] + q2[-1] * q1[:3] + np.cross(q1[:3], q2[:3])
+    return q
 
 
 class GpsToRelativePoseDFN:
@@ -278,7 +298,7 @@ def configure(logs, stream_names):
     graph.add_transform("imu0", "body0", t)
     # body0 - dgps0, constant, known before start
     t = cdff_envire.Transform()
-    t.transform.translation.fromarray(np.array([-0.3, 0.0, 0.53]))
+    t.transform.translation.fromarray(np.array([0.3, 0.0, -0.53]))
     t.transform.orientation.fromarray(np.array([0.0, 0.0, 0.0, 1.0]))
     graph.add_transform("dgps0", "body0", t)
     # imu0 - global_pose0, constant, known after start
