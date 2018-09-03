2ce742a3553105df50b157693c228a28644615f5
Alexander Fabisch <alexander.fabisch@dfki.de>
2018-09-03 13:48:38 +0200

    Parallelize data conversion and processing

--- cdff_dev/replay.py
+++ cdff_dev/replay.py
@@ -1,4 +1,5 @@
-from . import logloader, typefromdict
+import asyncio
+from . import typefromdict
 
 
 def replay_and_process(dfc, log_iterator):
@@ -18,3 +19,46 @@ def replay_and_process(dfc, log_iterator):
         obj = typefromdict.create_from_dict(typename, sample)
         dfc.process_sample(
             timestamp=timestamp, stream_name=stream_name, sample=obj)
+
+
+async def _sample_reader(log_iterator, queue):
+    for timestamp, stream_name, typename, sample in log_iterator:
+        obj = typefromdict.create_from_dict(typename, sample)
+        await queue.put((timestamp, stream_name, obj))
+
+    await queue.put(None)
+
+
+async def _process_sample(dfc, queue):
+    while True:
+        item = await queue.get()
+        if item is None:
+            break
+
+        timestamp, stream_name, obj = item
+        dfc.process_sample(
+            timestamp=timestamp, stream_name=stream_name, sample=obj)
+
+
+def replay_and_process_async(dfc, log_iterator):
+    """Replay log file(s) and feed them to DataFlowControl.
+
+    This version does conversion to objects and data flow control processing
+    in parallel.
+
+    Parameters
+    ----------
+    dfc : DataFlowControl
+        Configured processing components
+
+    log_iterator : Iterable
+        Iterable object that yields log samples in the correct temporal
+        order. The iterable returns in each step a quadrupel of
+        (timestamp, stream_name, typename, sample).
+    """
+    loop = asyncio.get_event_loop()
+    queue = asyncio.Queue(loop=loop)
+    producer_coro = _sample_reader(log_iterator, queue)
+    consumer_coro = _process_sample(dfc, queue)
+    loop.run_until_complete(asyncio.gather(producer_coro, consumer_coro))
+    loop.close()
