6bc9609d4e7d9a8d934150f3d2e39e19175a0eee
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2018-09-06 14:21:07 +0200

    added sherpa urdf model

--- examples/robot_viz.py
+++ examples/robot_viz.py
@@ -8,7 +8,7 @@ def main():
     #graph.add_frame("center")
 
     #cdff_envire.load_urdf(graph,"./sherpa_tt-master/urdf/sherpa.urdf",False,False,True);
-    cdff_envire.load_urdf(graph,"./bundle-sherpa_tt/data/urdf_sherpa_meshes_LQ/sherpa_tt.urdf",False,False,True);
+    cdff_envire.load_urdf(graph,"./urdf_sherpa_meshes_LQ/sherpa_tt.urdf",False,False,True);
     #cdff_envire.load_smurf(graph,"./sherpa_tt-master/smurf/sherpa.smurf",False,False,True);
     
 
