e64396b43a1033bb30361f040c66255194da77bb
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2018-10-07 22:02:22 +0200

    Simplify replay script

--- examples/replay_images.py
+++ examples/replay_images.py
@@ -1,42 +1,21 @@
-import glob
 from cdff_dev import dataflowcontrol, logloader, visualization_qtgraph
 
 
 def main():
     verbose = 2
-    connections = (
-        ("/hcru0/pt_stereo_rect/left.image", "result.leftImage"),
-        ("/hcru0/pt_stereo_rect/right.image", "result.rightImage"),
-        ("/hcru0/pt_stereo_sgm.depth", "result.depthImage")
-    )
-    stream_names = [
-        "/hcru0/pt_stereo_rect/left/image",
-        "/hcru0/pt_stereo_rect/right/image",
-        "/hcru0/pt_stereo_sgm/depth",
-    ]
+    filename = "test/test_data/logs/frames2.msg"
+    stream_names = ["/hcru0/pt_stereo_rect/left/image"]
     stream_aliases = {
-        "/hcru0/pt_stereo_rect/left/image": "/hcru0/pt_stereo_rect/left.image",
-        "/hcru0/pt_stereo_rect/right/image": "/hcru0/pt_stereo_rect/right.image",
-        "/hcru0/pt_stereo_sgm/depth": "/hcru0/pt_stereo_sgm.depth"
-    }
-    # Note that the logfiles are not in the repository because they are too
-    # large. Ask Alexander Fabisch about it.
-    log_folder = "logs/DLR_20180724/"
-    logfiles = [
-        sorted(glob.glob(log_folder + "recording_20180724-135036_hcru0_pt_stereo_rect_left_image_*.msg")),
-        sorted(glob.glob(log_folder + "recording_20180724-135036_hcru0_pt_stereo_rect_right_image_*.msg")),
-        sorted(glob.glob(log_folder + "recording_20180724-135036_hcru0_pt_stereo_sgm_depth_*.msg"))
-
-    ]
-    log_iterator = logloader.replay_files(logfiles, stream_names)
+        "/hcru0/pt_stereo_rect/left/image": "/hcru0/pt_stereo_rect/left.image"}
+    log_iterator = logloader.replay_logfile(filename, stream_names)
 
     dfc = dataflowcontrol.DataFlowControl(
-        nodes={}, connections=connections, stream_aliases=stream_aliases,
+        nodes={}, connections=(), stream_aliases=stream_aliases,
         verbose=verbose)
     dfc.setup()
 
     app = visualization_qtgraph.ImageVisualizerApplication(
-        "/hcru0/pt_stereo_sgm.depth")
+        "/hcru0/pt_stereo_rect/left.image")
     app.show_controls(log_iterator, dfc)
     app.exec_()
 
