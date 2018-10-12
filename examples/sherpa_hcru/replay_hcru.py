d7c7508f585bdbb68e1951e40e582b7daff00219
Alexander Fabisch <Alexander.Fabisch@dfki.de>
2018-10-12 14:20:57 +0200

    Add more examples

--- examples/sherpa_hcru/replay_hcru.py
+++ examples/sherpa_hcru/replay_hcru.py
@@ -57,8 +57,10 @@ def main():
         verbose=verbose)
     dfc.setup()
 
-    #app = imagevisualization.ImageVisualizerApplication("/hcru1/pt_color/left.image")
     app = imagevisualization.ImageVisualizerApplication("/hcru1/pt_stereo_rect/left.image")
+    #app = imagevisualization.ImageVisualizerApplication("/hcru1/pt_stereo_rect/right.image")
+    #app = imagevisualization.ImageVisualizerApplication("/hcru1/pt_color/left.image")
+    #app = imagevisualization.ImageVisualizerApplication("/hcru1/pt_stereo_sgm.depth")
     app.show_controls(log_iterator, dfc)
     app.exec_()
 
