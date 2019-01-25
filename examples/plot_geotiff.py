b8505488e4dd33a35baa11699b9803993b91a3ba
Alexander Fabisch <Alexander.Fabisch@dfki.de>
2019-01-25 13:39:36 +0100

    Show any GeoTIFF file

--- examples/plot_geotiff.py
+++ examples/plot_geotiff.py
@@ -1,9 +1,14 @@
+import sys
 import matplotlib.pyplot as plt
 from cdff_dev import io
 
 
 def main():
-    gtm = io.GeoTiffMap("test/test_data/maps/res1.tif", verbose=1)
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        filename = "test/test_data/maps/res1.tif"
+    gtm = io.GeoTiffMap(filename, verbose=1)
     origin, m = gtm.slice((250, 500), (600, 800))
     m.data.array_reference()[m.data.array_reference() == gtm.undefined] = 0.0
     plt.imshow(m.data.array_reference().squeeze())
