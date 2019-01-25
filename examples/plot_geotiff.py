ffd4137142cc583a4160f33dacc090b9a0093212
Alexander Fabisch <Alexander.Fabisch@dfki.de>
2019-01-25 13:50:35 +0100

    Crop map

--- examples/plot_geotiff.py
+++ examples/plot_geotiff.py
@@ -8,8 +8,12 @@ def main():
         filename = sys.argv[1]
     else:
         filename = "test/test_data/maps/res1.tif"
+    if len(sys.argv) > 5:
+        x_min, x_max, y_min, y_max = sys.argv[2:6]
+    else:
+        x_min, x_max, y_min, y_max = 250, 500, 600, 800
     gtm = io.GeoTiffMap(filename, verbose=1)
-    origin, m = gtm.slice((250, 500), (600, 800))
+    origin, m = gtm.slice((x_min, x_max), (y_min, y_max))
     m.data.array_reference()[m.data.array_reference() == gtm.undefined] = 0.0
     plt.imshow(m.data.array_reference().squeeze())
     plt.gray()
